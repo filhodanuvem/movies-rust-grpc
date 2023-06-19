798de365400ad58cec9d012098b3f240a437a08e
Claudson Oliveira <claudsonweb@gmail.com>
2023-06-19 14:05:56 +0100

    fix health

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ impl Movie for MovieService {
             year: 1999,
             genre: "Sci-Fi".to_string(),
             rating: "8.7".to_string(),
-            start_rating: "4.5".to_string(),
+            star_rating: "4.8".to_string(),
             runtime: "136".to_string(),
             cast: "Keanu Reeves, Laurence Fishburne".to_string(),
             image: "http://image.tmdb.org/t/p/w500//aOIuZAjPaRIE6CMzbazvcHuHXDc.jpg".to_string(),
@@ -35,7 +35,7 @@ impl Movie for MovieService {
             year: 2023,
             genre: "Animation".to_string(),
             rating: "9.7".to_string(),
-            start_rating: "4.5".to_string(),
+            star_rating: "4.9".to_string(),
             runtime: "136".to_string(),
             cast: "Donald Glover".to_string(),
             image: "http://image.tmdb.org/t/p/w500//8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg".to_string(),
@@ -46,7 +46,7 @@ impl Movie for MovieService {
             year: 2013,
             genre: "Drama".to_string(),
             rating: "8.7".to_string(),
-            start_rating: "4.5".to_string(),
+            star_rating: "4.1".to_string(),
             runtime: "136".to_string(),
             cast: "Joaquin Phoenix".to_string(),
             image: "http://image.tmdb.org/t/p/w500//eCOtqtfvn7mxGl6nfmq4b1exJRc.jpg".to_string(),
@@ -61,7 +61,7 @@ impl Movie for MovieService {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = env::var("PORT").unwrap_or("50051".to_string());
-    let addr = format!("127.0.0.1:{}", port).parse()?;
+    let addr = format!("0.0.0.0:{}", port).parse()?;
     let movie = MovieService::default();
     let movie = MovieServer::new(movie);
     let movie = tonic_web::enable(movie);
